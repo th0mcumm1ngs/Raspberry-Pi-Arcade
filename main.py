9b517cf884b9254ce994c6a670aa5efc2f1edc5c
Tom Cummings <rex4343@hotmail.com>
2021-02-09 22:23:38 +0000

    main.py updated
    Title widget added. Background colour updated.

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from tkinter import *
 root = Tk()
 
 root.title("RPi Arcade")
-root.config(bg = "white")
-root.attributes('-fullscreen', True) 
+root.config(bg = "#373737")
+root.attributes('-fullscreen', True)
+
+Title = Label(root, text = "Game Selection", fg = "#C7C7C7", bg = "#373737", font = ("SF Pro Display Ultralight", 120))
+Title.place(anchor = N, relx = 0.5)
 
 root.mainloop()
